081a1c62721d48563f77a374abb24293ab26d452
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2023-11-15 16:03:13 +0700

    Update SplashScreen.dart

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -33,30 +33,28 @@ class _SplashScreen extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff0C2F23),
-      body: SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  if (isLoading)
-                    LoadingAnimationWidget.inkDrop(
-                      color: Color(0xffA9BB86),
-                      size: 50,
-                    ),
-                  Image.asset(
-                    'assets/logo.png', // Replace 'your_image.png' with your actual image asset path
-                    width: 300.0, // Adjust the width as needed
-                    height: 300.0, // Adjust the height as needed
-                  ),
-                ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 300.0, // Adjust the width as needed
+              height: 300.0, // Adjust the height as needed
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Image.asset(
+                  'assets/logo.png', // Replace 'your_image.png' with your actual image asset path
+                ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 300), // Increase the spacing between image and loading animation
+            if (isLoading)
+              LoadingAnimationWidget.inkDrop(
+                color: Color(0xffA9BB86),
+                size: 50,
+              ),
+          ],
         ),
       ),
     );
