9f6994469907d52917ffde29e3a6a14a263d16d0
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2023-11-15 23:38:58 +0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -3,74 +3,95 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeContent(),
+    );
+  }
+}
+
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  int _selectedIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Harga Parkiran'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          height: MediaQuery.of(context).size.height / 2,
-          child: ParkingCard(),
-        ),
+      backgroundColor: Color(0xff0C2F23),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: <Widget>[
+          HomeContentPage(),
+          //List(),
+          // Parkir(),
+          // Profile(),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        backgroundColor: Color(0xff2E4D3E),
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.home),
             label: 'Info',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.car),
+            icon: Icon(Icons.car_rental),
             label: 'Parkir',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.people),
+            icon: Icon(Icons.person),
             label: 'Member',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.account_box),
             label: 'Profile',
           ),
         ],
+        currentIndex: _selectedIndex,
+        onTap: onTabTapped,
+        // Ganti dengan warna yang diinginkan
       ),
     );
   }
 }
 
-class ParkingCard extends StatelessWidget {
+class HomeContentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(16.0),
+    return Center(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Harga Parkiran',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        child: Card(
+          color: Colors.white, // Warna latar belakang card
+          elevation: 7.0, // Ketinggian shadow card
+          child: Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min
+              ,
+              children: <Widget>[
+                Text(
+                  'Joker',
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  'Joker adalah sebuah karakter penjahat superrrr fiksiadasdas4ewqa...',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-            Text(
-              'Tarif Parkir: Rp. 5,000 per jam',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              'Maksimum Biaya: Rp. 30,000',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Tambahkan logika untuk membayar parkiran atau navigasi ke halaman pembayaran
-                // Contoh: Navigator.push(context, MaterialPageRoute(builder: (context) => PaymentPage()));
-              },
-              child: Text('Bayar Parkiran'),
-            ),
-          ],
+          ),
         ),
       ),
     );
