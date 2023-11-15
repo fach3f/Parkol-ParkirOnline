3ed906f98d7a6800df8a93d982f7fd2490eda0e8
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2023-11-15 15:24:46 +0700

    Update SplashScreen.dart

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -8,6 +8,8 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreen extends State<SplashScreen> {
+  bool isLoading = true;
+
   void initState() {
     super.initState();
     splashScreenStart();
@@ -16,6 +18,10 @@ class _SplashScreen extends State<SplashScreen> {
   splashScreenStart() async {
     var duration = const Duration(seconds: 3);
     return Timer(duration, () {
+      setState(() {
+        isLoading = false;
+      });
+
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => Home()),
@@ -26,18 +32,31 @@ class _SplashScreen extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 31, 102, 34),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[ // Add spacing between text and image
-            Image.asset(
-              'assets/logo.png', // Replace 'your_image.png' with your actual image asset path
-              width: 300.0, // Adjust the width as needed
-              height: 300.0, // Adjust the height as needed
-            ),
-          ],
+      backgroundColor: const Color(0xff0C2F23),
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  if (isLoading)
+                    LoadingAnimationWidget.inkDrop(
+                      color: Color(0xffA9BB86),
+                      size: 50,
+                    ),
+                  Image.asset(
+                    'assets/logo.png', // Replace 'your_image.png' with your actual image asset path
+                    width: 300.0, // Adjust the width as needed
+                    height: 300.0, // Adjust the height as needed
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
