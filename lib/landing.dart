1b04355b925caa414210d79189d6d2075a9fecb7
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2023-11-22 20:56:20 +0700

    Update landing.dart

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,11 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:parkol/Login.dart';
+import 'package:parkol/Signup.dart';
+import 'package:parkol/kapasitas.dart';
+import 'package:parkol/map.dart';
+
+class Landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeContent(),
+    );
+  }
+}
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+class _HomeContentState extends State<HomeContent> {
+  int _selectedIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: <Widget>[
+          Home(),
+          map(),
+          Signup(),
+          Login(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flight_land),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            label: 'More',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_parking),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.auto_graph_rounded),
+            label: 'Data',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: onTabTapped,
+      ),
+    );
+  }
+}
 
 
 
-
-
-class landing extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +78,7 @@ class landing extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'Selamat Datang di \n      Aplikasi Kami',
+              'Selamat Datang di\n     Aplikasi Kami',
               style: GoogleFonts.goldman(
                 fontSize: 28,
                 color: Colors.white,
@@ -26,20 +88,19 @@ class landing extends StatelessWidget {
             SizedBox(height: 20),
             Text(
               'Menemukan Parkir Lebih Mudah!',
-              style: GoogleFonts.openSans(
+              style: GoogleFonts.goldman(
                 fontSize: 18,
                 color: Colors.white,
               ),
             ),
             SizedBox(height: 40),
             Image.asset(
-              'assets/logo1.png',  // Gantilah dengan path logo aplikasi Anda
+              'assets/logo.png',  // Gantilah dengan path logo aplikasi Anda
               width: 150,
               height: 150,
-              fit: BoxFit.scaleDown,
+              fit: BoxFit.contain,
             ),
             SizedBox(height: 40),
-
           ],
         ),
       ),
