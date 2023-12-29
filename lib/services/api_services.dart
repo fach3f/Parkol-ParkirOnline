ea419243c9949d8120d5916419430c4056c53673
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2023-12-29 21:50:28 +0700

    Update api_services.dart

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -24,3 +24,30 @@ class ApiService {
     }
   }
 }
+
+class apilogin {
+  static const String baseUrl = 'http://localhost:8080';
+
+  static Future<void> login(String email, String password) async {
+    final apiUrl = '$baseUrl/login'; // Ganti dengan endpoint API login yang sesuai
+
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        body: {'email': email, 'password': password},
+      );
+
+      if (response.statusCode == 200) {
+        print('Login successful');
+        // Handle response data if needed
+      } else {
+        print('Login failed with status code: ${response.statusCode}');
+        print('Response body: ${response.body}');
+        // Handle error response if needed
+      }
+    } catch (error) {
+      print('Error during login: $error');
+      // Handle other errors, such as network issues
+    }
+  }
+}
