3fe8a3827a334cbe943865a51dd11ac82c90119c
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2024-01-03 01:05:50 +0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:parkol/map.dart';
+import 'package:parkol/slo1.dart';
+import 'package:parkol/slo2.dart';
 
 class menu extends StatelessWidget {
   @override
@@ -18,11 +21,36 @@ class menu extends StatelessWidget {
       backgroundColor: const Color(0xff0C2F23),
       body: ListView(
         children: <Widget>[
-          ListWidget(gambar: "assets/slot.png", judul: "Pengunjung"),
-          Divider(color: Colors.white, height: 10.0), // Pemisah
-          ListWidget(gambar: "assets/maps.png", judul: "Denah Parkir"),
-          Divider(color: Colors.white, height: 5.0), // Pemisah
-          ListWidget(gambar: "assets/kasir.png", judul: "Pendapatan"),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Lantai2()),
+              );
+            },
+            child: ListWidget(gambar: "assets/slot.png", judul: "Pengunjung"),
+          ),
+          Divider(color: Colors.white, height: 10.0),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Lantai1()),
+              );
+            },
+            child: ListWidget(gambar: "assets/maps.png", judul: "Denah Parkir"),
+          ),
+          Divider(color: Colors.white, height: 5.0),
+          InkWell(
+            onTap: () {
+              // Handle tap for "Pendapatan"
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Pendapatan()),
+              );
+            },
+            child: ListWidget(gambar: "assets/kasir.png", judul: "Pendapatan"),
+          ),
         ],
       ),
     );
@@ -38,7 +66,7 @@ class ListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xff0C2F23), // Set the background color here
+      color: const Color(0xff0C2F23),
       padding: EdgeInsets.all(30.0),
       child: Center(
         child: Row(
@@ -57,4 +85,3 @@ class ListWidget extends StatelessWidget {
     );
   }
 }
-
